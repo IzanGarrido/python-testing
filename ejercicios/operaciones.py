589d7f65bd62a0fe7b051594c5256bfdf574f837
Izan <izaneteyoly672@gmail.com>
2024-06-10 19:20:48 +0200

    errores corregidos

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -2,10 +2,10 @@ def suma(x,y):
     return x + y
 
 def resta(x,y):
-    return x + y
+    return x - y
 
 def div(x,y):
-    return x + y
+    return x / y
 
 def mult(x,y):
-    return x + y
+    return x * y
